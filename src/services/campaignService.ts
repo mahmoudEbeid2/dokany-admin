aba1d6f2bedc2cf05306855f591641839d078ad4
mahmoudEbeid2 <mahmoud.ebeid2002@gmail.com>
2025-08-18 20:19:51 +0300

    solve seller

--- src/services/campaignService.ts
+++ src/services/campaignService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://dokany-api-production.up.railway.app';
+const API_BASE_URL = import.meta.env.VITE_API ;
 
 // Create axios instance with default config
 const api = axios.create({
